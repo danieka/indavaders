2ff84708ae9c7917fbd84975507517374afa4717
Daniel Karlsson <danieka@kth.se>
2014-04-30 16:14:41 +0200

    More changes

---
+++ src/Indavaders.java
@@ -0,0 +1,42 @@
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Indavaders extends BasicGame
+{
+	public Indavaders(String gamename)
+	{
+		super(gamename);
+	}
+
+	@Override
+	public void init(GameContainer gc) throws SlickException {}
+
+	@Override
+	public void update(GameContainer gc, int i) throws SlickException {}
+
+	@Override
+	public void render(GameContainer gc, Graphics g) throws SlickException
+	{
+		g.drawString("Howdy!", 100, 100);
+	}
+
+	public static void main(String[] args)
+	{
+		try
+		{
+			AppGameContainer appgc;
+			appgc = new AppGameContainer(new Indavaders("Indavaders"));
+			appgc.setDisplayMode(640, 480, false);
+			appgc.start();
+		}
+		catch (SlickException ex)
+		{
+			Logger.getLogger(Indavaders.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
