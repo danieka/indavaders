91331b51cc40d23a8f992fb907144b8c5283800d
Pielen <pielbusch@me.com>
2014-05-05 13:42:39 +0200

    Added files

--- src/Indavaders.java
+++ src/Indavaders.java
@@ -1,42 +1,48 @@
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.BasicGame;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Graphics;
-import org.newdawn.slick.SlickException;
 
-public class Indavaders extends BasicGame
-{
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Indavaders extends StateBasedGame{
+	
+	public static int siffra;
+	public static AppGameContainer appgc;
+	
 	public Indavaders(String gamename)
 	{
 		super(gamename);
 	}
 
-	@Override
-	public void init(GameContainer gc) throws SlickException {}
-
-	@Override
-	public void update(GameContainer gc, int i) throws SlickException {}
-
-	@Override
-	public void render(GameContainer gc, Graphics g) throws SlickException
-	{
-		g.drawString("Howdy!", 100, 100);
-	}
 
 	public static void main(String[] args)
 	{
 		try
 		{
-			AppGameContainer appgc;
+			//AppGameContainer appgc;
 			appgc = new AppGameContainer(new Indavaders("Indavaders"));
 			appgc.setDisplayMode(640, 480, false);
+			appgc.setTargetFrameRate(60);
 			appgc.start();
+			
 		}
 		catch (SlickException ex)
 		{
 			Logger.getLogger(Indavaders.class.getName()).log(Level.SEVERE, null, ex);
 		}
+	}
+
+
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		this.addState(new StartState());
+		this.addState(new GameState());
+		this.addState(new EndState());
+		
+	}
+	
+	public static void exit(){
+		appgc.exit();
 	}
 }
