aeab33c0c4748525de3a900827f1e6624a354f36
Daniel Karlsson <danieka@kth.se>
2014-05-13 17:43:38 +0200

    Added lots of comments.

--- src/ai/Task.java
+++ src/ai/Task.java
@@ -3,6 +3,11 @@ package ai;
 import main.Fleet;
 import main.Planet;
 
+/**
+ * This class represents a task that the AI need to perform during the next turn.
+ * @author danieka
+ *
+ */
 public class Task implements Comparable<Task> {
 	private float score;
 	private Planet destination;
@@ -23,18 +28,35 @@ public class Task implements Comparable<Task> {
 		return (int)Math.ceil(this.score+(this.score / this.shipsNecessary) - arg0.score+(arg0.score / arg0.shipsNecessary));
 	}
 
+	/**
+	 * Get the planet associated with the task.
+	 * @return
+	 */
 	public Planet getPlanet() {
 		return destination;
 	}
 
+	/**
+	 * Get the score, that measures necessity of the task.
+	 * @return
+	 */
 	public float getScore() {
 		return score+(score / shipsNecessary);
 	}
 	
+	/**
+	 * Number of ships needed to perform this task.
+	 * @return
+	 */
 	public int getShipsNecessary() {
 		return shipsNecessary;
 	}
 
+	/**
+	 * Decrease the number of ships necessary.
+	 * This happens when a fleet is assigned to the task.
+	 * @param size
+	 */
 	public void decreaseShipsNecessary(int size) {
 		shipsNecessary -= size;
 	}
